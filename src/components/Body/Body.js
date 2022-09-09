baedde61bc92217a471a8c2d2e07329912f71138
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-09-09 15:25:59 +0200

    articles

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,43 +6,45 @@ let id = 1;
 
 const Body = ({ data }) => {
   return (
-    <section className="container is-fluid has-background-primary">
-      <div className="columns">
-        {data.map((el) => {
-          return (
-            <div
-              key={id++}
-              className="column is-family-sans-serif has-background-light my-6 mx-1"
-            >
-              <article className="media">
-                <figure className="media-left pt-6 mt-6">
-                  <p className="image is-128x128">
-                    <img src={el.urlToImage} />
-                  </p>
-                </figure>
-                <div className="media-content">
-                  <div className="content">
-                    <p className="is-family-sans-serif">
-                      <strong>{el.author}</strong> <br />
-                      <small>
-                        <a href={el.url} target="_blank">
-                          {el.url}
-                        </a>
-                      </small>{" "}
-                      <br />
-                      <small>{el.publishedAt}</small>
-                      <br />
-                      <strong>{el.title}</strong>
-                      <br />
-                      <br />
-                      {el.content}
+    <section className="section is-fluid has-background-primary-dark mt-6	">
+      <div className="container">
+        <div className="columns">
+          {data.map((el) => {
+            return (
+              <div
+                key={id++}
+                className="column is-pulled-left is-family-primary has-background-light my-6 mx-1"
+              >
+                <article className="media">
+                  <figure className="media-left pt-6 mt-6">
+                    <p className="image is-96x96">
+                      <img src={el.urlToImage} />
                     </p>
+                  </figure>
+                  <div className="media-content">
+                    <div className="content ">
+                      <p className="is-family-primary ">
+                        <strong>{el.author}</strong> <br />
+                        <small>
+                          <a href={el.url} target="_blank">
+                            {el.url}
+                          </a>
+                        </small>{" "}
+                        <br />
+                        <small>{el.publishedAt}</small>
+                        <br />
+                        <strong>{el.title}</strong>
+                        <br />
+                        <br />
+                        {el.content}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </article>
-            </div>
-          );
-        })}
+                </article>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "bulma/css/bulma.css";
 import Body from "../Body/Body";
+import Pagination from "bulma/css/bulma.css";
 
 const Navbar = () => {
   const BASE_URL = "https://newsapi.org/v2/everything";
   const [data, setData] = useState([]);
   const [topic, setTopic] = useState("auto-industry");
+  const [page, setPage] = useState(1);
   const getData = () => {
     axios
       .get(
-        `${BASE_URL}/?q=${topic}&pageSize=3&apiKey=01e6292bd6294b649bb8675dbc01e555`
+        `${BASE_URL}/?q=${topic}&pageSize=3&page=${page}&apiKey=01e6292bd6294b649bb8675dbc01e555`
       )
       .then((res) => setData(res.data.articles));
   };
@@ -18,33 +20,74 @@ const Navbar = () => {
   useEffect(() => {
     getData();
   }, [topic]);
+  useEffect(() => {
+    getData();
+  }, [page]);
   return (
-    <div className="has-background-info">
-      <button
-        className="button is-link is-light mx-6 my-6 px-6"
-        onClick={() => setTopic("formula1")}
-      >
-        Formula 1
-      </button>
-      <button
-        className="button is-light mx-6 my-6 px-6"
-        onClick={() => setTopic("verstappen")}
-      >
-        Verstappen
-      </button>
-      <button
-        className="button is-black mx-6 my-6 px-6"
-        onClick={() => setTopic("hamilton")}
-      >
-        Hamilton
-      </button>
-      <button
-        className="button is-link mx-6 my-6 px-6"
-        onClick={() => setTopic("leclerc")}
-      >
-        Leclerc
-      </button>
+    <div className="has-background-info-light">
+      <div>
+        <button
+          className="button has-background-primary-dark has-text-light mx-6 mt-6 px-6"
+          onClick={() => setTopic("formula1")}
+        >
+          Formula 1
+        </button>
+        <button
+          className="button has-background-primary-dark has-text-light mx-6 mt-6 px-6"
+          onClick={() => setTopic("verstappen")}
+        >
+          Verstappen
+        </button>
+        <button
+          className="button has-background-primary-dark has-text-light mx-6 mt-6 px-6"
+          onClick={() => setTopic("hamilton")}
+        >
+          Hamilton
+        </button>
+        <button
+          className="button has-background-primary-dark has-text-light mx-6 mt-6 px-6"
+          onClick={() => setTopic("leclerc")}
+        >
+          Leclerc
+        </button>
+      </div>
       <Body data={data} />
+      <nav
+        className="pagination is-medium py-3 px-4 has-background-success-dark"
+        role="navigation"
+        aria-label="pagination"
+      >
+        <ul className="pagination-list">
+          <li>
+            <a
+              className="pagination-link"
+              aria-label="Page 1"
+              aria-current="page"
+              onClick={() => setPage(1)}
+            >
+              1
+            </a>
+          </li>
+          <li>
+            <a
+              className="pagination-link"
+              aria-label="Goto page 2"
+              onClick={() => setPage(2)}
+            >
+              2
+            </a>
+          </li>
+          <li>
+            <a
+              className="pagination-link"
+              aria-label="Goto page 3"
+              onClick={() => setPage(3)}
+            >
+              3
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
